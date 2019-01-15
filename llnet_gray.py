1d273132584fcfea8d20255f4e4fce2650c87bf9
dlrudco <37071556+dlrudco@users.noreply.github.com>
2019-01-15 16:41:02 +0900

    changed weight dimension
    ..

--- llnet_gray.py
+++ llnet_gray.py
@@ -9,8 +9,7 @@ from PIL import Image
 def pause():
     programPause = input("Press the <ENTER> key to continue...")
 
-n_hidden = [2000, 1600, 1200]; 
-
+n_hidden = [867, 578, 289]; 
 
 n_input =   289;
 datasize = 8370;
